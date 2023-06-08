63db98be77dc74bc5017ad5d255317b2e44955d0
jcbcouch <jcbcouch@gmail.com>
2023-06-07 19:08:49 -0700

    init

---
+++ comments.py
@@ -0,0 +1,23 @@
+
+
+#This is a comment.
+print("Hello, World!")
+print("Hello, World!") #This is a comment 
+
+#Since Python will ignore string literals that are not assigned to a variable, you can add 
+# a multiline string (triple quotes) in your code, and place your comment inside it:
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+'''
+This is a comment
+written in
+more than just one line
+'''
+
+
+x = 5
+y = "Hello, World!"
---
+++ variables.py
@@ -0,0 +1,26 @@
+#Variables are containers for storing data values.
+#Python has no command for declaring a variable.
+#A variable is created the moment you first assign a value to it.   
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Variables do not need to be declared with any particular type, and can even 
+# change type after they have been set.
+z = 4       # z is of type int
+z = "Sally" # z is now of type str
+print(z)
+
+#If you want to specify the data type of a variable, this can be done with casting.
+1 = str(3)    # x will be '3'
+2 = int(3)    # y will be 3
+3 = float(3)  # z will be 3.0 
+
+
+#You can get the data type of a variable with the type() function.
+a = 5
+b = "John"
+print(type(a))
+print(type(b)) 
